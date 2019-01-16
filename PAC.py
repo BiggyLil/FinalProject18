9a515a93c03f2aac7e689f34bc53d6a905b98f64
estephany14049 <estephanyjimenez777@gmail.com>
2019-01-16 16:49:22 -0500

    Update PAC.py
    code

--- PAC.py
+++ PAC.py
@@ -13,70 +13,109 @@ pygame.display.set_caption('PACMAN')
 px=400
 py=200
 
-ghost_h= 88
-ghost_w= 75
+ghost_h= 50
+ghost_w= 50
 
 dx= 700
 dy= 300
 
+pacman_w = 50
+pacman_h = 50
+
 lives=3
 
-class Ghosts:
-  def __init__(self, name):
-    self.name=name
-    # self.image = ''
-    self.x = 50
-    self.y = 50
+class Pacman(pygame.sprite.Sprite):
+  """defines paccy"""
+  def __init__(self):
+    self.rect.x=0
+    self.rect.y=0
+    super.__init__()
+    self.image= pygame.image.load('PAAAAAAAAAAAAACCCCCCCCCC.png')
+    self.rect=self.image.get_rect()
+
+  def display(self):
+    screen.blit(pac, (px, py))
+ 
+  def move(self):
+   if event.type == pygame.KEYDOWN:
+    if pygame.key.name(event.key) == 'up':
+      self.rect.y-=1
+    if pygame.key.name(event.key) == 'down':
+      self.rect.y+=1
+    if pygame.key.name(event.key) == 'right':
+      self.rect.x+=1
+    if pygame.key.name(event.key) == 'left':
+      self.rect.x-=1
+
+   if px < 0:
+     px=0
+   elif px > 800-pacman_w:
+      px=800-pacman_w
+   if py < 0:
+      py=0
+   elif py > 400-pacman_h:
+     py=400-pacman_h
+
+pac=Pacman()
+
+class Ghosts(pygame.sprite.Sprite):
+  """defines ghosties"""
+  def __init__(self):
+    self.rect.x = 50
+    self.rect.y = 50
+    super.__init__()
     if name== 'derbie':
       self.image= pygame.image.load('ghooooooooost.png')
     if name== 'dobe':
       self.image= pygame.image.load('omgggggggggg.png')
     if name== 'dobie':
       self.image= pygame.image.load('ahhhhhhhh.png')
+    self.rect=self.image.get_rect()
+
+  def display(self):
+    screen.blit(self.image, (self.rect.x, self.rect.y))
+ 
+  def move(self, Pacman):
+    distx= Pacman.rect.x-self.rect.x
+    disty= Pacman.rect.x-self.rect.y
+    if random.random()<.02:
+      self.rect.x+=distx/20
+      self.rect.y+=disty/20
+
+    if self.rect.x < 0:
+      self.rect.x= 0
+    elif self.rect.x > 800-ghost_w:
+      self.rect.x= 800-ghost_w
+    if self.rect.y < 0:
+     self.rect.y= 0
+    elif self.rect.y > 400-ghost_h:
+      self.rect.y= 400-ghost_h
+
+derbie= Ghosts()
+dobe= Ghosts()
+dobie =Ghosts()
+ 
+class Dot(pygame.sprite.Sprite):
+  """defines dot"""
+  def __init__(self):
+    self.rect.x= 700
+    self.rect.y= 300
+    super.__init__()
+    self.image= pygame.image.load('dot.png')
+    self.rect= self.image.get_rect()
   
   def display(self):
-    screen.blit(self.image, (self.x, self.y))
- 
-  def move(self):
-    distx= px-self.x
-    disty= py-self.y
-    if random.random()<.02:
-      self.x+=distx/20
-      self.y+=disty/20
+    screen.blit(self.image, (self.rect.x,self.rect.y))
 
-    if self.x < 0:
-      self.x= 0
-    elif self.x > 800-ghost_w:
-      self.x= 800-ghost_w
-    if self.y < 0:
-     self.y= 0
-    elif self.y > 400-ghost_h:
-      self.y= 400-ghost_h
 
-derbie= Ghosts('derbie')
-dobe= Ghosts('dobe')
-dobie =Ghosts('dobie')
-
-pac = pygame.image.load('PAAAAAAAAAAAAACCCCCCCCCC.png') 
-  
 running=True 
 
-while running=True:
+while running==True:
   for event in pygame.event.get():
     if event.type == pygame.QUIT:
       running = False
 
   screen.fill(background_colour)
-  
-  if event.type == pygame.KEYDOWN:
-    if pygame.key.name(event.key) == 'up':
-      py-=1
-    if pygame.key.name(event.key) == 'down':
-      py+=1
-    if pygame.key.name(event.key) == 'right':
-      px+=1
-    if pygame.key.name(event.key) == 'left':
-      px-=1
   
   derbie.move()
   dobe.move()
@@ -85,31 +124,14 @@ while running=True:
   derbie.display()
   dobe.display()
   dobie.display()
-  
 
-  screen.blit(pac, (px, py))
-  pacman_w = 75
-  pacman_h = 75
-  if px < 0:
-     px=0
-  elif px > 800-pacman_w:
-    px=800-pacman_w
-  if py < 0:
-    py=0
-  elif py > 400-pacman_h:
-    py=400-pacman_h
+  pac.display()
+  pac.move()
 
-  dot= pygame.image.load('dot.png')
+  dot.display()
 
-  def display_dot():
-    screen.blit(dot, (dx,dy))
-  
-  display_dot()
-
-  if px==dx and py==dy:
-    dx= random.randint(0,700)
-    dy= random.randint(0,300)
+    # dx= random.randint(0,700)
+    # dy= random.randint(0,300)
 
   pygame.display.flip()
-
 
